155a45af4f4c2e4bdc309a22051018b8ce24a2a3
Carl Chen <ccf.developer@gmail.com>
2017-04-07 11:34:12 +0800

    wechat sdk version: 1.7.7

---
+++ WechatOpenSDK/WechatOpenSDK.h
@@ -0,0 +1,21 @@
+//
+//  WechatOpenSDK.h
+//  WechatOpenSDK
+//
+//  Created by Carl on 7/4/2017.
+//  Copyright © 2017 nswefrog. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WechatOpenSDK.
+FOUNDATION_EXPORT double WechatOpenSDKVersionNumber;
+
+//! Project version string for WechatOpenSDK.
+FOUNDATION_EXPORT const unsigned char WechatOpenSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WechatOpenSDK/PublicHeader.h>
+
+#import "WXApi.h"
+#import "WechatAuthSDK.h"
+
